f10fdf8e12bb21521ff3ad10c5b119c41dc67597
Brian McRoberts <brimcrob@gmail.com>
2020-04-02 17:39:39 +0100

    update 2 for loops

---
+++ venv/Loops.py
@@ -0,0 +1,9 @@
+fruits = ["apples", "oranges", "pears", "cherries", "grapes"]
+
+
+for fruit in fruits:
+    print("Would you like {} ?".format(fruit))
+
+
+for number in range(1,11):
+    print("Number {}".format(number))
