0155f4b37505c5f3c33ae6a7e2850eb022caddcd
Brian McRoberts <brimcrob@gmail.com>
2020-04-08 23:01:19 +0100

    update 2 for loops

--- venv/Lists.py
+++ venv/Lists.py
@@ -1,6 +1,15 @@
-fruits = ["peaches", "pears","apples"]
+fruits = ["apples","peaches", "pears","apples"]
 years = [3, "1998", 2.5, 987, "1994"]
 
+print("apple" in fruits)
+print(fruits.count("apples"))
+print(fruits.index("apples"))
+
+
+
+
+
+"""
 print (fruits, years)
 
 
@@ -13,5 +22,11 @@ print(fruits)
 fruits.remove("oranges")
 print(fruits)
 
-fruits.pop(0)
-print(fruits)
+#fruits.pop(0)
+#print(fruits)
+
+numbers = [5,1928,4,17,68]
+numbers.sort()
+print(numbers)
+"""
+
