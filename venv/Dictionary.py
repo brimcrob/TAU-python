0e79fd7e1e34d56c3d9373814304e3715a084cef
Brian McRoberts <brimcrob@gmail.com>
2020-04-08 23:00:56 +0100

    update 2 for loops

---
+++ venv/Dictionary.py
@@ -0,0 +1,25 @@
+stuff = {"food": 15, "energy": 100, "enemies": 3}
+
+new_items ={"rocks": 4, "arrows": 18}
+stuff.update(new_items)
+print(stuff)
+
+
+
+
+'''
+print(stuff.setdefault("food"))
+print(stuff)
+print(stuff.setdefault("friends", 123))
+print(stuff)
+'''
+
+#print(stuff.popitem())
+#print(stuff)
+
+
+
+#stuff.get("food")
+#print(stuff.get("food"))
+#print(stuff.items())
+#print(stuff.keys())
